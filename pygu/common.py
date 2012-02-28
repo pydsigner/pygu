31ba7da54bac809d504b8fff828c7ec22ab45b37
Daniel Foerster <pydsigner@gmail.com>
2012-02-28 15:20:53 -0600

    Added more center blitters to common.py

--- pygu/common.py
+++ pygu/common.py
@@ -6,14 +6,31 @@ from pgpu.math_utils import Vector
 def get_ext(fl):
     return fl.split('.')[-1].lower()
 
+
+def _blitter(loc, target, source, dest, area, special_flags):
+    toblit = source.subsurface(area) if area != None else source
+    target.blit(toblit, loc + dest, special_flags = special_flags)
+
+def center_blit(target, source, dest = (0, 0), area=None, special_flags=0):
+    '''
+    Blits surface @source to the center of surface @target. Takes the normal 
+    Surface.blit() flags; however, @dest is used as an offset.
+    '''
+    loc = Vector(target.get_size()) / 2 - Vector(toblit.get_size()) / 2
+    _blitter(loc, target, source, dest, area, special_flags)
+
 def vcenter_blit(target, source, dest = (0, 0), area=None, special_flags=0):
     '''
-    Blits surface @source in a vertically centered manner to surface @target. 
-    Takes the normal Surface.blit() flags; however, @dest is used as an offset.
+    The same as center_blit(), but only centers vertically.
     '''
-    toblit = source.subsurface(area) if area != None else source
-    
-    loc = (Vector(0, target.get_height() / 2) + dest - 
+    loc = (Vector(0, target.get_height() / 2) - 
             Vector(0, toblit.get_height() / 2))
-    
-    target.blit(toblit, loc, special_flags = special_flags)
+    _blitter(loc, target, source, dest, area, special_flags)
+
+def hcenter_blit(target, source, dest = (0, 0), area=None, special_flags=0):
+    '''
+    The same as center_blit(), but only centers horizontally.
+    '''
+    loc = (Vector(target.get_width() / 2, 0) - 
+            Vector(toblit.get_width() / 2, 0))
+    _blitter(loc, target, source, dest, area, special_flags)
