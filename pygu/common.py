b6c96aa7df5e8075c357bf66d2435a23be99b648
Daniel Foerster <pydsigner@gmail.com>
2012-02-28 15:26:39 -0600

    Fixed blitters

--- pygu/common.py
+++ pygu/common.py
@@ -9,28 +9,28 @@ def get_ext(fl):
 
 def _blitter(loc, target, source, dest, area, special_flags):
     toblit = source.subsurface(area) if area != None else source
-    target.blit(toblit, loc + dest, special_flags = special_flags)
+    target.blit(toblit, loc(target, toblit) + dest, special_flags=special_flags)
 
 def center_blit(target, source, dest = (0, 0), area=None, special_flags=0):
     '''
     Blits surface @source to the center of surface @target. Takes the normal 
     Surface.blit() flags; however, @dest is used as an offset.
     '''
-    loc = Vector(target.get_size()) / 2 - Vector(toblit.get_size()) / 2
+    loc = lambda d, s: Vector(d.get_size()) / 2 - Vector(s.get_size()) / 2
     _blitter(loc, target, source, dest, area, special_flags)
 
 def vcenter_blit(target, source, dest = (0, 0), area=None, special_flags=0):
     '''
     The same as center_blit(), but only centers vertically.
     '''
-    loc = (Vector(0, target.get_height() / 2) - 
-            Vector(0, toblit.get_height() / 2))
+    loc = lambda d, s: (Vector(0, d.get_height() / 2) - 
+            Vector(0, s.get_height() / 2))
     _blitter(loc, target, source, dest, area, special_flags)
 
 def hcenter_blit(target, source, dest = (0, 0), area=None, special_flags=0):
     '''
     The same as center_blit(), but only centers horizontally.
     '''
-    loc = (Vector(target.get_width() / 2, 0) - 
-            Vector(toblit.get_width() / 2, 0))
+    loc = lambda d, s: (Vector(d.get_width() / 2, 0) - 
+            Vector(s.get_width() / 2, 0))
     _blitter(loc, target, source, dest, area, special_flags)
